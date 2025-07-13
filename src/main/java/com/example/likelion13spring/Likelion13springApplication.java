40b5b3364dd934326b2137b8d46c185d8c2113cb
YoonisKim <runheng1209@gmail.com>
2025-07-13 17:18:41 +0900

    Initial commit

---
+++ src/main/java/com/example/likelion13spring/Likelion13springApplication.java
@@ -0,0 +1,13 @@
+package com.example.likelion13spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Likelion13springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Likelion13springApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/likelion13spring/Likelion13springApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.likelion13spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Likelion13springApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
