e75f66693dd4596f91a88d737ef1895cfe8ec821
YoonisKim <runheng1209@gmail.com>
2025-08-03 21:32:28 +0900

    feat: week18 과제 제출

---
+++ src/main/java/com/example/likelion13spring/domain/Mapping/ProductOrders.java
@@ -0,0 +1,31 @@
+package com.example.likelion13spring.domain.Mapping;
+
+import com.example.likelion13spring.domain.Orders;
+import com.example.likelion13spring.domain.Product;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductOrders {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Orders orders;
+
+    private Integer quantity; // 구매 수량
+}
---
+++ src/main/java/com/example/likelion13spring/domain/Orders.java
@@ -0,0 +1,37 @@
+package com.example.likelion13spring.domain;
+
+import com.example.likelion13spring.domain.Mapping.ProductOrders;
+import com.example.likelion13spring.enums.DeliverStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Orders extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private DeliverStatus deliverStatus; // 배송상태
+
+    @ManyToOne
+    @JoinColumn(name ="buyer_id")
+    private Member buyer;
+
+    @OneToMany(mappedBy = "orders", cascade = CascadeType.ALL)
+    private List<ProductOrders> productOrders;
+
+    @OneToOne(mappedBy = "orders", cascade = CascadeType.ALL)
+    private Coupon coupon;
+}
+
