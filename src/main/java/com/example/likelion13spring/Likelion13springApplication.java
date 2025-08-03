732da1c50a5ace01589c3f436e8248f2e09d9de0
YoonisKim <runheng1209@gmail.com>
2025-08-03 21:32:28 +0900

    feat: week18 과제 제출

--- src/main/java/com/example/likelion13spring/Likelion13springApplication.java
+++ src/main/java/com/example/likelion13spring/Likelion13springApplication.java
@@ -2,8 +2,10 @@ package com.example.likelion13spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Likelion13springApplication {
 
 	public static void main(String[] args) {
